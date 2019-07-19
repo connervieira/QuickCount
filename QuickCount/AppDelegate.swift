97c4817ca9f5451a4c47b35746af4531b0836245
Conner Vieira <vieirast@icloud.com>
2019-07-19 17:30:56 -0400

    Initial Commit

---
+++ QuickCount/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickCount
+//
+//  Created by Conner Vieira on 7/19/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
